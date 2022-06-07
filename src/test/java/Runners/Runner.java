7451e475bf152d84649e9d7b5fe275108d3eb2a4
Senita Isovic <admin@Senitas-MacBook-Pro.local>
2022-06-07 11:55:28 +0200

    updated

--- src/test/java/Runners/Runner.java
+++ src/test/java/Runners/Runner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         features = "src/test/resources/createNewUser.feature",
-        //plugin = {"pretty"}
+        plugin = {"pretty"},
         glue = {"StepDefinitions"}
 )
 public class Runner {
